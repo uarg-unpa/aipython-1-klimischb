ecaf22d648d345600d04b4c4d97e0913c9c4fd85
klimischb <147439659+klimischb@users.noreply.github.com>
2023-10-20 21:24:10 +0000

    actualizar

--- entrega/test.py
+++ entrega/test.py
@@ -1,1 +1,10 @@
-
+print("viernes20AIPYTHON") #Invocación  #Argumentos:Lo que está entre parentesis                         
+print() #imprime una lunea blanca
+print("taller","AIPYTHON","VIERNES") #Argumentos Posicionales:Separados por comas
+print("taller","AIPYTHON","VIERNES",sep="",end="_")
+print("taller","AIPYTHON","VIERNES",end="") #Argumentos de Palabras Clave #sep= de separación, separador es una espacio en blanco #end=de ter,omación, barra invertida n
+print("recibido","vamos",sep="",end="\n")
+#print("la función "print"")"ln"
+print("la función \"print\"...")
+print('la función "print"')
+# CLEAR para borrar todo
