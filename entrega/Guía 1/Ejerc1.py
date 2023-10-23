ed9a395622e9ca545a0406c0dabe2695df8764dc
klimischb <147439659+klimischb@users.noreply.github.com>
2023-10-22 21:37:17 -0300

    Update Ejerc1.py

--- entrega/Guía 1/Ejerc1.py
+++ entrega/Guía 1/Ejerc1.py
@@ -2,3 +2,13 @@ print("Las máquinas me sorprenden con mucha frecuencia") #Escribir un programa 
 print()#Mostrar una línea vacía
 print("Para eso sirve la Utopia, para caminar") #Mostrar tus propios mensajes
 print('"23" 23') #Mostrar el número “23” y 23 ¿Qué sucedió?
+print('Una computadora puede ser llamada "inteligente" si logra engañar a una persona haciéndole creer que
+es un humano')
+print(Barbara, Klimisch, ?) #Escribir un programa que muestre tu nombre, apellido y tu edad como
+argumentos separados.
+print(Barbara, Klimisch, ?) #Modificar el programa anterior para que en vez de que los argumentos estén
+separados por espacios, se unan por un guión bajo. Tip usar sep.
+h. Escribir un programa que muestre tres argumentos: calle, número y código
+postal. Deben estar separados por una tabulación. Tip \t
+i. Modificar el ejercicio anterior para que muestre cada uno de los argumentos
+en una nueva línea.
