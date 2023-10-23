c7e7f3bbbbb0d56b940c90ec6f32f224ad14b2a9
klimischb <147439659+klimischb@users.noreply.github.com>
2023-10-23 18:21:24 +0000

    actualizar 9

--- entrega/test.py
+++ entrega/test.py
@@ -1,14 +1,14 @@
 print("viernes20AIPYTHON") #Invocación  #Argumentos:Lo que está entre parentesis                         
-print() #imprime una lunea blanca
-print("taller","AIPYTHON","VIERNES") #Argumentos Posicionales:Separados por comas
+print() #imprime una línea blanca
+print("taller","AIPYTHON","VIERNES") #Argumentos Posicionales:Separados por comas, pero me da espacios
 print("taller","AIPYTHON","VIERNES",sep="",end="_")
-print("taller","AIPYTHON","VIERNES",end="") #Argumentos de Palabras Clave #sep= de separación, separador es una espacio en blanco #end=de ter,omación, barra invertida n
-print("recibido","vamos",sep="",end="\n")
-#print("la función "print"")"ln"
+print("taller","AIPYTHON","VIERNES",end="") #Argumentos de Palabras Clave #sep= de separación, separador es una espacio en blanco #end=de termimación, barra invertida n
+print("recibido","vamos",sep="_") #sep"_" lo que esta entre comillas usa de separación
+print("recibido","vamos",sep="",end="\n") #print("la función "print"")"ln"
 print("la función \"print\"...")
 print('la función "print"')
 # CLEAR para borrar todo
 print("la función print\t hola")
-print("FIN") 
+print("FIN")
 #CADENAS NUMEROS BOOLEANOS NONE
 #En la terminal si colocamos la paabra Python, nos permite hacer operaciones directamente ej 4+5, damos enter y no da el reasultado 
