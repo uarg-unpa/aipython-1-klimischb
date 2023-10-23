070264e5d00edfcd9b176b7da832d452b314af5b
klimischb <147439659+klimischb@users.noreply.github.com>
2023-10-23 19:26:12 +0000

    ACT3

--- entrega/Operaciones.py
+++ entrega/Operaciones.py
@@ -8,6 +8,7 @@ print(22**33)
 print(22+33+45)
 #Las variables: no pueden tener espacios, ni guion medio o signo menos, no pueden empezar con un numero, si con minuscula, si con guion bajo, no se pden llamar como un palabra reservada o fx(ej. print)
 #Pueden ser numeros o cadenas(ej color=verde)
+#Las variables siempre tienen que estar definidas previamente
 #VARIABLE(una caja para guardar los literales(numeros, golianos y cadenas)) 
 #valor=7 tiene un nombre o identificador, el operador, asignación y un valor ("un algo" adentro de la caja)
 #valor=10 el operador asignacion da valor al algo dentro dela caja
@@ -15,3 +16,6 @@ print(22+33+45)
 #13
 color="rojo"
 print(color) #esta línea es ignorada por el intérprete, se conoce como comentario
+color=3+5
+print("color")
+print(edad) #la variable no esta definida
