46004e9f07358f40a35c140c5174ae6d45bdbea0
klimischb <147439659+klimischb@users.noreply.github.com>
2023-10-23 19:17:47 +0000

    ACTUAL31

--- entrega/Operaciones.py
+++ entrega/Operaciones.py
@@ -5,3 +5,11 @@ print(22/33) #DIVISION COMUN
 print(22%33) #MODULO O RESTO 
 print(22//33) #DIVISION ENTERA: CUNATO ENTEROS ENTRAN EN ELUMERO
 print(22**33)
+print(22+33+45)
+#Las variables: no pueden tener espacios, ni guion medio o signo menos, no pueden empezar con un numero, si con minuscula, si con guion bajo, no se pden llamar como un palabra reservada o fx(ej. print)
+#Pueden ser numeros o cadenas(ej color=verde)
+#VARIABLE(una caja para guardar los literales(numeros, golianos y cadenas)) 
+#valor=7 tiene un nombre o identificador, el operador, asignación y un valor ("un algo" adentro de la caja)
+#valor=10 el operador asignacion da valor al algo dentro dela caja
+#valor=valor + 3
+#13
