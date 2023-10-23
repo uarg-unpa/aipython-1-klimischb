04b213a2b6ff44177d8ec375f1b1a04e44586a36
klimischb <147439659+klimischb@users.noreply.github.com>
2023-10-23 10:47:24 -0300

    Update Ejerc1.py
    bk3

--- entrega/Guía 1/Ejerc1.py
+++ entrega/Guía 1/Ejerc1.py
@@ -8,7 +8,5 @@ print("Barbara", "Klimisch","?") #Escribir un programa que muestre tu nombre, ap
 argumentos separados.
 print("Barbara","Klimisch","?",sep="") #Modificar el programa anterior para que en vez de que los argumentos estén
 separados por espacios, se unan por un guión bajo. Tip usar sep.
-
-Escribir un programa que muestre tres argumentos: calle, número y código postal. Deben estar separados por una tabulación. Tip \t
-Modificar el ejercicio anterior para que muestre cada uno de los argumentos
-en una nueva línea.
+print("calle\t", número\t","CP\t") #Escribir un programa que muestre tres argumentos: calle, número y código postal. Deben estar separados por una tabulación. Tip \t
+Modificar el ejercicio anterior para que muestre cada uno de los argumentos en una nueva línea.
