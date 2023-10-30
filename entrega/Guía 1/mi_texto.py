ba1693198532eed2ba1f2937fe9f1ca4fc0e1bc7
klimischb <147439659+klimischb@users.noreply.github.com>
2023-10-30 19:54:57 +0000

    455

--- entrega/Guía 1/mi_texto.py
+++ entrega/Guía 1/mi_texto.py
@@ -2,6 +2,15 @@ mi_texto="python"
 print(mi_texto)
 una_parte= mi_texto[0:3]
 
-edad=70
-If (edad>16 and edad<=75)
-    Print (“el elector puede votar”)
+#edad=70
+#If(edad>16 and edad<=75):
+    #Print (“el elector puede votar”)
+
+numero1=(int(input("ingrese un numero:")))
+numero2=input("ingrese numero:")
+numero2=int(numero2)
+if numero1>numero2:
+   mayor=numero1
+else:
+    mayor=numero2
+    print(mayor)
