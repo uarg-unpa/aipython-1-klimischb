1d81c802c4bf2e94baafdceb7ea4724fa11cd33f
klimischb <147439659+klimischb@users.noreply.github.com>
2023-11-03 21:37:51 +0000

    viernes

--- entrega/Guía 1/Practico1_2parte.py
+++ entrega/Guía 1/Practico1_2parte.py
@@ -10,3 +10,18 @@ altura="1.72"
 print(altura)
 vuelo="1803"
 print(vuelo)
+#
+#
+cont=0
+while(cont<=10)
+print(cont)
+#
+promedio=0
+cont=0
+suma=0
+while(cont<3)
+    nota=int(input("ingrese nota"))
+    suma=suma+nota
+    cont=cont+1
+promedio=suma/cont
+print(f"el promedio es {promedo}")
