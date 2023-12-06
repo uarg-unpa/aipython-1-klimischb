779258ac5ddac7ee5fbef9850523b24762000db8
klimischb <147439659+klimischb@users.noreply.github.com>
2023-12-06 13:23:05 +0000

    vamo5

---
+++ entrega/Guía 1/CA.py
@@ -0,0 +1,12 @@
+## Yo le digo la altura al triángulo (introduciendo un entero)
+n = int(input("Introduce la altura del triángulo (entero positivo): "))
+for i in range(n):
+    for j in range(i+1):
+        print("*", end="")
+    print("")
+#
+## Genera un triángulo de 7 niveles
+for i in range(7):
+    for j in range(i+1):
+        print("*", end="")
+    print("")
