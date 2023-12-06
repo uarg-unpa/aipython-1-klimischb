b3bc0f9c14a3c6ceafe25990aed5767d676dbc99
klimischb <147439659+klimischb@users.noreply.github.com>
2023-12-06 12:29:51 +0000

    Volví1

--- entrega/Guía 1/TP3.py
+++ entrega/Guía 1/TP3.py
@@ -1,13 +1,14 @@
 #TP3 #Lo logramos
 #1 Iterar de 0 a 100 usando un bucle while y mostrar dichos números.
-cont=1
+cont=0
 while(cont<=100):
-    print(cont,end="")
+    print(cont)
     cont=cont+1
 #
 #2 Tomar el ejercicio 1 y realizarlo con un bucle for, tip usar range. los números deben salir uno al lado del otro.
 for num in range (0, 101):
-    print(num)
+    print(num,end="")
+    print()
 #3 Iterar de 10 a 0 usando un bucle while y un bucle for.
 #Bucle while de 10 a o
 cont=10
