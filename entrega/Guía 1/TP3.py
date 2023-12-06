16b8000817b90bf7e277b5cdba6b0432fee45bb3
klimischb <147439659+klimischb@users.noreply.github.com>
2023-12-06 12:59:18 +0000

    vamo1

--- entrega/Guía 1/TP3.py
+++ entrega/Guía 1/TP3.py
@@ -1,15 +1,15 @@
 #TP3 #Lo logramos
-#1 Iterar de 0 a 100 usando un bucle while y mostrar dichos números.
+### 1 Iterar de 0 a 100 usando un bucle while y mostrar dichos números.
 cont=0
 while(cont<=100):
     print(cont)
     cont=cont+1
 #
-#2 Tomar el ejercicio 1 y realizarlo con un bucle for, tip usar range. los números deben salir uno al lado del otro.
+### 2 Tomar el ejercicio 1 y realizarlo con un bucle for, tip usar range. los números deben salir uno al lado del otro.
 for num in range (0, 101,1):
     print(num,end="")
     print(end="")
-#3 Iterar de 10 a 0 usando un bucle while y un bucle for.
+### 3 Iterar de 10 a 0 usando un bucle while y un bucle for.
 #Bucle while de 10 a o
 cont=10
 while(cont>=0):
@@ -20,5 +20,11 @@ while(cont>=0):
 #bucle for
 for num in range (11, -1,-1):
     print(num)
-#4 Escribir un programa que pida al usuario dos números enteros e imprima todos los números entre ellos.
+### 4 Programa que pide al usuario dos números enteros e imprime todos los números entre ellos.
+num1=int(input("Ingrese el primer numero"))
+num2=int(input("Ingrese el segundo numero"))
+for num in range (num1,num2,1):
+    print(num) 
+
+#
 #5 Escribe un bucle que haga siete llamadas a print(), de modo que obtengamos UN TRIANGULO
